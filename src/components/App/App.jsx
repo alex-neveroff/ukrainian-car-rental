cf496bc3f9d71f2c685478723362eab9732f4b53
Alexandr Neveroff <114777678+alex-neveroff@users.noreply.github.com>
2023-09-13 09:35:06 +0300

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import { Container } from './App.styled';
+
+function App() {
+  return (
+    <Container>
+      <p>REACT APP TEMPLATE</p>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import App from 'components/App/App';
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
